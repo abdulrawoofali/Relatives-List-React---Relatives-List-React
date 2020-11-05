8c878511d786b61cfb28d510aa7a8f3250a88ca5
abdulrawoofali <rawoofabd41@gmail.com>
2020-11-05 15:04:59 +0530

    added list of items

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+  let relativeList = ['relativeListItem1':'I will be visting Kasol','relativeListItem2':'I will be visting Himachal','relativeListItem3':'I will be visting Delhi'];
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+              <ol>
+                    {relativeList.map(list =>
+                   
+                   <li key={list.key}>{list.description}</li>
+                        
+                    )}
+                </ol>
             </div>
         )
     }
