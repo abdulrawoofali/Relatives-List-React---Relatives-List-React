45d5cebd19f3a44b5af3cd9a08c778abe9b326c3
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-09 23:05:16 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-  let relativeList = ['relativeListItem1':'I will be visting Kasol','relativeListItem2':'I will be visting Himachal','relativeListItem3':'I will be visting Delhi'];
-    render() {
-
-        return(
-            <div id="main">
-              <ol>
-                    {relativeList.map(list =>
-                   
-                   <li key={list.key}>{list.description}</li>
-                        
-                    )}
-                </ol>
-            </div>
-        )
-    }
+	render() {
+		let relatives = ["abdul", "abcd", "ijkl"];
+		return (
+			<>
+				<ol key="relativeList">
+					{relatives.map((relative, index) => (
+						<li key={"relativeListItem" + (index + 1)}>{relative}</li>
+					))}
+				</ol>
+			</>
+		);
+	}
 }
-
 
 export default App;
